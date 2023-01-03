f25f35ff5eb5c42bdbe3c728ceae51989e53bdc7
Anes Benmerzoug <a.benmerzoug@appliedai.de>
2023-01-03 17:16:08 +0100

    Add set_random_seed() helper function

---
+++ src/experiments/utils.py
@@ -0,0 +1,22 @@
+import os
+import random
+
+import numpy as np
+import torch
+
+__all__ = ["set_random_seed"]
+
+
+def set_random_seed(seed: int) -> None:
+    """Taken verbatim from:
+    https://koustuvsinha.com//practices_for_reproducibility/
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    os.environ["PYTHONHASHSEED"] = str(seed)
