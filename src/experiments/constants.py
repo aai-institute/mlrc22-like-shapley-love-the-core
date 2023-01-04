65761e59e114394697211f3a214cc8c5bbaf1aac
Anes Benmerzoug <a.benmerzoug@appliedai.de>
2023-01-04 13:55:44 +0100

    Define OUTPUT_DIR constant

--- src/experiments/constants.py
+++ src/experiments/constants.py
@@ -3,6 +3,7 @@ from pathlib import Path
 __all__ = [
     "RANDOM_SEED",
     "DATA_DIR",
+    "OUTPUT_DIR",
     "BREAST_CANCER_OPENML_ID",
     "HOUSE_VOTING_OPENML_ID",
 ]
@@ -11,6 +12,7 @@ RANDOM_SEED = 16
 
 ROOT_DIR = Path(__file__).parent.parent.parent
 DATA_DIR = ROOT_DIR / "data"
+OUTPUT_DIR = ROOT_DIR / "output"
 
 BREAST_CANCER_OPENML_ID = 43611
 HOUSE_VOTING_OPENML_ID = 56
