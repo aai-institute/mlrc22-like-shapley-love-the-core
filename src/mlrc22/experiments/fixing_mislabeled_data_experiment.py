5d73c4da651560a3f924d148cccaf2b53b5fec00
Miguel de Benito Delgado <m.debenito@appliedai.de>
2023-02-02 22:21:20 +0100

    Another ugly hack to delay solving of LPs for mislabeled data experiment

--- src/mlrc22/experiments/fixing_mislabeled_data_experiment.py
+++ src/mlrc22/experiments/fixing_mislabeled_data_experiment.py
@@ -1,11 +1,13 @@
 import io
 import logging
 from contextlib import redirect_stderr
+from time import time
 
 import numpy as np
 import pandas as pd
+import pydvl
 from pydvl.reporting.scores import compute_removal_score
-from pydvl.utils import Utility
+from pydvl.utils import Utility, init_parallel_backend
 from pydvl.utils.config import ParallelConfig
 from pydvl.value.least_core import montecarlo_least_core
 from pydvl.value.results import ValuationResult
@@ -45,19 +47,35 @@ def run():
     n_iterations = 5000
     logger.info(f"Using number of iterations {n_iterations}")
 
-    n_repetitions = 5
+    n_repetitions = 10
     logger.info(f"Using number of repetitions {n_repetitions}")
 
-    n_jobs = 4
+    n_jobs = 56
     logger.info(f"Using number of jobs {n_jobs}")
 
     model = GaussianNB()
 
     all_values_df = None
 
-    all_scores = []
-
     random_state = np.random.RandomState(RANDOM_SEED)
+    all_scores = []
+    delayed_values = []
+    parallel_backend = init_parallel_backend(parallel_config)
+
+    def _fun(u, testing_utility, data, method_name, scorer_name, flip_percentage):
+        if method_name in ("Random", "TMC Shapley"):
+            values = data
+        else:
+            values = pydvl.value.least_core.montecarlo._solve_linear_problem(u, data)
+        return dict(
+            values=values,
+            method_name=method_name,
+            scorer_name=scorer_name,
+            flip_percentage=flip_percentage,
+            testing_utility=testing_utility,
+        )
+
+    fun = parallel_backend.wrap(_fun)
 
     with tqdm_logging_redirect():
         for flip_percentage in tqdm(
@@ -91,6 +109,7 @@ def run():
                             model=model,
                             scoring=scorer_name,
                             enable_cache=False,
+                            show_warnings=False
                         )
 
                         testing_utility = Utility(
@@ -98,6 +117,7 @@ def run():
                             model=model,
                             scoring=scorer_name,
                             enable_cache=False,
+                            show_warnings=False
                         )
 
                         if method_name == "Random":
@@ -107,7 +127,6 @@ def run():
                         elif method_name == "Least Core":
                             values = montecarlo_least_core(
                                 training_utility,
-                                epsilon=0.0,
                                 n_iterations=n_iterations,
                                 n_jobs=n_jobs,
                                 config=parallel_config,
@@ -130,46 +149,75 @@ def run():
                                     config=parallel_config,
                                     mode=ShapleyMode.TruncatedMontecarlo,
                                 )
-                        # Sort values in increasing order
-                        values.sort()
-                        # The data points with flipped labels should have the lowest valuation
-                        # So to verify that we compute the percentage of data points
-                        # with the lowest valuation that correspond to the flipped data points
-                        lowest_value_indices = values.indices[: len(flipped_indices)]
-                        assert lowest_value_indices.shape == flipped_indices.shape
-                        flip_accuracy = np.in1d(
-                            lowest_value_indices, flipped_indices
-                        ).mean()
-
-                        scores = compute_removal_score(
-                            u=testing_utility,
-                            values=values,
-                            percentages=removal_percentages,
-                            remove_best=False,
-                            progress=False,
+                        delayed_values.append(
+                            dict(
+                                u=training_utility,
+                                testing_utility=testing_utility,
+                                data=values,
+                                method_name=method_name,
+                                scorer_name=scorer_name,
+                                flip_percentage=flip_percentage,
+                            )
                         )
-                        scores["method"] = method_name
-                        scores["scorer"] = scorer_name
-                        scores["flip_percentage"] = flip_percentage
-                        scores["flip_accuracy"] = flip_accuracy
-                        all_scores.append(scores)
 
-                        # Save raw values
-                        column_name = f"{method_name}_{flip_percentage}"
-                        df = (
-                            values.to_dataframe(column=column_name)
-                            .drop(columns=[f"{column_name}_stderr"])
-                            .T
-                        )
-                        df = df[sorted(df.columns)]
-                        df["method"] = method_name
-                        df["scorer"] = scorer_name
-                        df["flip_percentage"] = flip_percentage
+    logger.info(f"Waiting for {len(delayed_values)} promised values")
+    start = time()
+    resolved_values = parallel_backend.get(
+            [fun.remote(**p) for p in delayed_values]
+            )
+    logger.info(
+            f"Resolved {len(resolved_values)} promised values in "
+            f"{time() - start:.2f} seconds"
+            )
 
-                        if all_values_df is None:
-                            all_values_df = df.copy()
-                        else:
-                            all_values_df = pd.concat([all_values_df, df])
+    for result in resolved_values:
+
+        values = result["values"]
+        method_name = result["method_name"]
+        scorer_name = result["scorer_name"]
+        flip_percentage = result["flip_percentage"]
+        testing_utility = result["testing_utility"]
+
+        # Sort values in increasing order
+        values.sort()
+        # The data points with flipped labels should have the lowest valuation
+        # So to verify that we compute the percentage of data points
+        # with the lowest valuation that correspond to the flipped data points
+        lowest_value_indices = values.indices[: len(flipped_indices)]
+        assert lowest_value_indices.shape == flipped_indices.shape
+        flip_accuracy = np.in1d(
+            lowest_value_indices, flipped_indices
+        ).mean()
+
+        scores = compute_removal_score(
+            u=testing_utility,
+            values=values,
+            percentages=removal_percentages,
+            remove_best=False,
+            progress=False,
+        )
+        scores["method"] = method_name
+        scores["scorer"] = scorer_name
+        scores["flip_percentage"] = flip_percentage
+        scores["flip_accuracy"] = flip_accuracy
+        all_scores.append(scores)
+
+        # Save raw values
+        column_name = f"{method_name}_{flip_percentage}"
+        df = (
+            values.to_dataframe(column=column_name)
+            .drop(columns=[f"{column_name}_stderr"])
+            .T
+        )
+        df = df[sorted(df.columns)]
+        df["method"] = method_name
+        df["scorer"] = scorer_name
+        df["flip_percentage"] = flip_percentage
+
+        if all_values_df is None:
+            all_values_df = df.copy()
+        else:
+            all_values_df = pd.concat([all_values_df, df])
 
     scores_df = pd.DataFrame(all_scores)
 
